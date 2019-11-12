8cc4ea8f22172ec499a9ea4fe8407db09d8b189d
Rikard-Sut <mailarikardskoglundnu@gmail.com>
2019-11-12 13:47:00 +0100

    Add project files.

---
+++ AbstractHuman/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractHuman
+{
+    class Student : Human
+    {
+        private string grade;
+
+        public Student(string firstName, string lastName, string grade) : base(firstName, lastName)
+        {
+            this.grade = grade;
+        }
+        public string Grade
+        {
+            get { return this.grade; }
+            set { this.grade = value; }
+        }
+    }
+}
---
+++ AbstractHuman/Worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractHuman
+{
+    class Worker : Human
+    {
+        private int weekSalary;
+        private int workHoursPerDay;
+
+        public Worker(string firstName, string lastName, int weekSalary, int workHoursPerDay) : base(firstName, lastName)
+        {
+            this.weekSalary = weekSalary;
+            this.workHoursPerDay = workHoursPerDay;
+        }
+        public int WeekSalary
+        {
+            get { return this.weekSalary; }
+            set { this.weekSalary = value; }
+        }
+        public int WorkHoursPerDay
+        {
+            get { return this.workHoursPerDay; }
+            set { this.workHoursPerDay = value; }
+        }
+        public double MoneyPerHour()
+        {
+            double money = weekSalary / 5 / workHoursPerDay;
+            return money;
+        }
+
+    }
+}
